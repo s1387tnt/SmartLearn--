9fb85217ee29977891b3aa909da746808f9d7db2
Yuanyi1129 <a0966659177@gmail.com>
2025-08-15 17:47:01 +0800

    feat 導覽列修改 測驗連接

--- static/config.js
+++ static/config.js
@@ -1,1 +1,9 @@
-const API_BASE = "https://49b008f0a53f.ngrok-free.app";
+let API_BASE;
+    // 如果是本地端就用本地 API
+if (window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost") {
+    API_BASE = "http://127.0.0.1:8000";
+} else {
+    // 自動抓當前網域（包含 ngrok）
+    API_BASE = window.location.origin;
+}
+console.log("偵測到的 API_BASE =", API_BASE);
